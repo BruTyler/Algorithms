905849ec629c1299ded833cee8fa37bfe90ffc5f
Anton Churkin <afrg@yandex.ru>
2022-02-14 12:42:46 +0500

    Застёжка-молния

---
+++ basic/zip-array.js
@@ -0,0 +1,57 @@
+/*
+Застёжка-молния
+Ограничение времени	1 секунда
+Ограничение памяти	64Mb
+Ввод	стандартный ввод или input.txt
+Вывод	стандартный вывод или output.txt
+Даны два массива чисел длины n. Составьте из них один массив длины 2n, в котором числа из входных массивов чередуются (первый — второй — первый — второй — ...). При этом относительный порядок следования чисел из одного массива должен быть сохранён.
+
+Формат ввода
+В первой строке записано целое число n –— длина каждого из массивов, 1 ≤ n ≤ 1000.
+
+Во второй строке записано n чисел из первого массива, через пробел.
+
+В третьей строке –— n чисел из второго массива.
+
+Значения всех чисел –— натуральные и не превосходят 1000.
+
+Формат вывода
+Выведите 2n чисел из объединённого массива через пробел.
+*/
+
+const _readline = require('readline');
+
+const _reader = _readline.createInterface({
+  input: process.stdin
+});
+
+const _inputLines = [];
+let _curLine = 0;
+
+_reader.on('line', line => {
+  _inputLines.push(line);
+});
+
+process.stdin.on('end', solve);
+process.on('SIGINT', function () {
+  solve();
+  process.exit();
+});
+
+function readLine() {
+  return _inputLines[_curLine++];
+}
+
+function solve() {
+  const n = Number(readLine());
+  const A = readLine().split(" ").map(Number);
+  const B = readLine().split(" ").map(Number);
+  const C = []
+
+  for (let i = 0; i < n; i++) {
+    C.push(A[i]);
+    C.push(B[i])
+  }
+
+  console.log(C.join(" "));
+}
